7f204d2817c2e89909885062c7bc9a0fba38b5e2
Clipph <kheijhackt2@gmail.com>
2025-07-16 04:09:50 +0800

    Designed a homepage for the repertoire tracker app

--- Repertoire_Tracker_Backend/settings.py
+++ Repertoire_Tracker_Backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'api',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -123,9 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # This enables the global static folder
+]
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
