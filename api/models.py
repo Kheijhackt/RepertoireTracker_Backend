fffce745471dea31f873efdb485ac91aa82b0c43
Clipph <kheijhackt2@gmail.com>
2025-07-15 21:49:42 +0800

    Altered default repertoire data

--- api/models.py
+++ api/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.hashers import make_password, check_password
 
+def default_repertoire():
+        return {"allPieces": []}
+
 class AppUser(models.Model):
     username = models.CharField(max_length=150, primary_key=True)
     display_name = models.CharField(max_length=150)
     password = models.CharField(max_length=128)  # Hashed
-    repertoire = models.JSONField(default=dict, blank=True)
+    repertoire = models.JSONField(default=default_repertoire(), blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(null=True, blank=True)  # Username change only
 
