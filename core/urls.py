dcba1b3e723b8e95a815749cbe1c19423a7e2598
Clipph <kheijhackt2@gmail.com>
2025-07-19 14:52:29 +0800

    Made screens for privacy policy and terms of use. Download button has now vanished.

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('privacy/', privacy, name='privacy'),
+    path('terms/', terms, name='terms'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def privacy(request):
+    return render(request, 'privacy.html')
+
+def terms(request):
+    return render(request, 'terms.html')
